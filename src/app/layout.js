83204efd06838e9a62d5fde3f7916419d39890c6
Jagdish Patil <patil.jagdish222@gmail.com>
2025-01-06 15:41:08 +0530

    favicon icon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable}  ${InterMono.variable} antialiased`}
       >
